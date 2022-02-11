7e6a0740807d8c839ef4a3f591dd45edc48836b6
hanniabu <hanniabu@gmail.com>
2022-02-11 15:26:07 -0500

    add details modal

--- _includes/js/base.js
+++ _includes/js/base.js
@@ -2,3 +2,53 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+
+var resourceDetailModal = document.getElementById('resourceDetailModal');
+resourceDetailModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget;
+  // Extract info from data-bs-* attributes
+  var link = button.getAttribute('data-bs-link');
+  var title = button.getAttribute('data-bs-title');
+  var creator = button.getAttribute('data-bs-creator');
+  var description = button.getAttribute('data-bs-desc');
+  var categories = button.getAttribute('data-bs-categories');
+  var socials = button.getAttribute('data-bs-socials');
+  var docs = button.getAttribute('data-bs-docs');
+
+  updateValue("rdmLink", link, "link");
+  updateValue("rdmTitle", title, "text");
+  updateValue("rdmCreator", creator, "text");
+  updateValue("rdmDescription", description, "text");
+  updateValue("rdmCategories", categories, "text");
+  updateValue("rdmSocials", socials, "text");
+  updateValue("rdmDocs", docs, "link");
+})
+
+function isValid(val) {
+  if (val !== "" && val !== " " && val !== undefined && val !== null) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function updateValue(id, val, type) {
+  let el = document.getElementById(id);
+  if (isValid(val)) {
+    if (type == "link") {
+      el.href = val;
+    }
+    if (type == "text") {
+      el.innerHTML = val;
+    }
+    el.classList.remove("d-none");
+  } else {
+    el.classList.add("d-none");
+  }
+}
+
+
+
+
